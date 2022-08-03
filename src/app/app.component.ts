76f956c6caf677e341e606f44cfa51bb5d06504c
hfht <darth.nater1@gmail.com>
2022-08-03 14:20:59 -0400

    added baseUrl env var, moved html into ts files, first pass at a dark theme, rxjs added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,17 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template:`
+  <div style="display: flex; align-items: center; flex-direction: column; margin-top: 20vh">
+    <h1>wassup mellos</h1>
+    <div>
+      <ul>
+        <li><a routerLink="/home" routerLinkActive="active" ariaCurrentWhenActive="page">goto home</a></li>
+        <li><a routerLink="/vox-pop" routerLinkActive="active" ariaCurrentWhenActive="page">goto vox pop</a>
+      </ul>
+    </div>
+    <router-outlet></router-outlet>
+  </div>`,
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
--- src/app/vox-pop/vox-pop.component.ts
+++ src/app/vox-pop/vox-pop.component.ts
@@ -1,20 +1,52 @@
-import { Component, OnInit } from '@angular/core';
-import { createClient, TumblrClient } from 'tumblr.js'
-import { LoggerService } from '../services/logger.service';
+import { Component, OnInit } from "@angular/core";
+import { LoggerService } from "../services/logger.service";
+import { HttpClient } from "@angular/common/http";
+import { environment } from "../../environments/environment";
+import { Observable } from "rxjs";
 
 @Component({
-  selector: 'app-vox-pop',
-  templateUrl: './vox-pop.component.html',
-  styleUrls: ['./vox-pop.component.scss']
+  selector: "app-vox-pop",
+  // templateUrl: "./vox-pop.component.html",
+  template: `
+  <div style="display: flex; flex-direction: column; align-items: center">
+    <p>write your message bro</p>
+    <textarea #submissionArea></textarea>
+    <button (click)="submit(submissionArea.value)">submit</button>
+  </div>`,
+  styleUrls: ["./vox-pop.component.scss"]
 })
 export class VoxPopComponent implements OnInit {
-  title = 'Vox Pop';
+  title = "Vox Pop";
+  userIP: string = '';
+  userAgent: string = '';
+  getRequest = this.http.get(environment.baseUrl + "/vox-pop", {responseType: "text"})
 
-  constructor(private log: LoggerService) {
-    
+  constructor(
+    private log: LoggerService, 
+    private http: HttpClient,
+    ){
   }
 
   ngOnInit(): void {
+    this.getUserIP().subscribe(response => {
+        this.userIP = response;
+    })
+    this.userAgent = window.navigator.userAgent;
   }
 
+  getUserIP(): Observable<string> {
+    return this.http.get("https://api.ipify.org", {responseType: "text"});
+  }
+
+  submit(submissionText: string) {
+    let dateTime = new Date();
+    const body = {
+      userIP: this.userIP,
+      userAgent: this.userAgent,
+      timestamp: dateTime,
+      submission: submissionText,
+    }
+    console.log('msg received: ', JSON.stringify(body));
+    this.http.post(environment.baseUrl + "/vox-pop", JSON.stringify(body), {responseType: "text"}).subscribe();
+  }
 }
