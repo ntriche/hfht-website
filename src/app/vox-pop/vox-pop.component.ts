dfbd6006e3551fce27d3d81fead03131497066f2
hfht <darth.nater1@gmail.com>
2022-10-05 16:32:41 -0400

    Add formgroup stuff for submission area

--- src/app/vox-pop/vox-pop.component.ts
+++ src/app/vox-pop/vox-pop.component.ts
@@ -1,26 +1,26 @@
 import { Component, OnInit } from "@angular/core";
+import { FormControl, FormGroup } from "@angular/forms";
 import { lastValueFrom } from "rxjs";
 import { VoxPopService, Post } from "./vox-pop.service";
 
 @Component({
   selector: "app-vox-pop",
-  template: `
-    <div style="display: flex; flex-direction: column; align-items: center">
-      <p>Write your submission here:</p>
-      <textarea name="submissionArea" #submissionArea required minlength="2" class="submission-box"></textarea>
-      <button (click)="submit(submissionArea.value)">submit</button>
-    </div>`,
+  templateUrl: "./vox-pop.component.html",
   styleUrls: ["./vox-pop.component.scss"]
 })
 export class VoxPopComponent implements OnInit {
   title = "Vox Pop";
+
+  voxPopFormGroup = new FormGroup({
+    submissionArea: new FormControl()
+  });
 
   constructor(private voxPopService: VoxPopService) {}
 
   ngOnInit(): void {}
 
   async submit(submissionText: string) {
-    // converts an observable into a promise
+    // lastValueFrom converts an observable into a promise
     let userIP = await lastValueFrom(this.voxPopService.getUserIP());
 
     let dateTime = new Date();
@@ -29,7 +29,8 @@ export class VoxPopComponent implements OnInit {
       timestamp: dateTime,
       submission: submissionText,
     }
-
+    
     this.voxPopService.sendSubmission(post).subscribe(res => console.log('Message from server:', res));
+    this.voxPopFormGroup.controls.submissionArea.patchValue('');
   }
 }
