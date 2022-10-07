7dfb387f40376d98bcbc6bdca256da7f12a53894
hfht <darth.nater1@gmail.com>
2022-10-07 00:17:50 -0400

    Began implementation and styling of dashboard

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,14 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import { voxPop } from '../vox-pop/interface/vox-pop.interface';
+import { DashboardService } from './dashboard.service';
 
+enum postStatus {
+  NotReviewed = 0,
+  Denied,
+  Approved,
+  SuperApproved,
+}
+interface reviewPost {
+  status: postStatus,
+  pop: voxPop
+}
 @Component({
   selector: 'app-dashboard',
-  template: `
-    <span>welcome to the dash</span>
-  `,
+  templateUrl: './dashboard.component.html', 
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
-  constructor() {}
+  posts: reviewPost[] = []  
 
-  ngOnInit(): void {}
+  constructor(private dashboardService: DashboardService) {}
+
+  ngOnInit(): void {
+    this.dashboardService.getPosts().subscribe({
+      next: v => {
+        let pops: voxPop[] = JSON.parse(v) as voxPop[];
+        for (let i = 0; i < pops.length; i++) {
+          const post: reviewPost = {
+            status: postStatus.NotReviewed,
+            pop: pops[i]
+          }
+          this.posts.push(post);
+        }
+        console.log(this.posts);
+      },
+      error: (e) => {throw e},
+    });
+  }
 }
--- src/app/dashboard/dashboard.service.ts
+++ src/app/dashboard/dashboard.service.ts
@@ -1,9 +1,17 @@
+import { HttpHeaders, HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DashboardService {
+  httpHeaders = new HttpHeaders().set('Content-Type', 'application/json; charset=utf-8');
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  getPosts(): Observable<string> {
+    return this.httpClient.get(environment.baseUrl + '/dashboard', {responseType: 'text', headers: this.httpHeaders})
+  }
 }
