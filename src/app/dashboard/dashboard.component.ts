b7f63d627e77d8a36cfcecd739f4b99e5bb34a25
hfht <darth.nater1@gmail.com>
2022-10-22 14:35:54 -0400

    Big commit

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -12,13 +12,17 @@ interface reviewPost {
   status: postStatus,
   pop: voxPop
 }
+
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html', 
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
-  posts: reviewPost[] = []  
+  unmoderatedPosts: reviewPost[] = [];
+  approvedPosts: reviewPost[] = [];
+  rejectedPosts: reviewPost[] = [];
+
 
   constructor(private dashboardService: DashboardService) {}
 
@@ -31,11 +35,52 @@ export class DashboardComponent implements OnInit {
             status: postStatus.NotReviewed,
             pop: pops[i]
           }
-          this.posts.push(post);
+          this.unmoderatedPosts.push(post);
         }
-        console.log(this.posts);
+        console.log(this.unmoderatedPosts);
       },
       error: (e) => {throw e},
     });
+  }
+
+  handlePost(post: reviewPost, index: number, newStatus: postStatus) {
+    if (newStatus === post.status) {
+      throw `Cannot change a post to new status - post already is that status! ${post}`;
+    }
+    switch (post.status) {
+      case postStatus.NotReviewed:
+        this.unmoderatedPosts.splice(index, 1);
+        break;
+      case postStatus.Denied:
+        this.rejectedPosts.splice(index, 1);
+        break;
+      case postStatus.Approved:
+        this.approvedPosts.splice(index, 1);
+        break;
+      case postStatus.SuperApproved:
+        this.approvedPosts.splice(index, 1);
+        break;
+      default:            
+        throw `Post has a non-postStatus status! ${post}`;
+    }
+
+    switch (newStatus) {
+      case postStatus.NotReviewed:
+        throw `Post cannot be pushed back into unmoderated array! ${post}`;
+      case postStatus.Denied:
+        this.rejectedPosts.push(post);
+        break;
+      case postStatus.Approved:
+        this.approvedPosts.push(post);
+        break;
+      case postStatus.SuperApproved:
+        this.approvedPosts.unshift(post);
+        break;
+      default:            
+        throw `Post has an invalid newStatus! ${post}`; 
+    }
+
+    post.status = newStatus;
+    return;
   }
 }
