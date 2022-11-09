1ff8a93e4ffd4e6fd36fb8b8ad5184ff07094ed0
hfht <darth.nater1@gmail.com>
2022-11-09 16:11:15 -0500

    Add modules, imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,21 +7,29 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
 import { HomeComponent } from './home/home.component';
 import { HttpClientModule } from '@angular/common/http';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { ReactiveFormsModule } from '@angular/forms'
-
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatDialogModule } from '@angular/material/dialog';
+import { DashboardEditPopupComponent } from './dashboard-edit-popup/dashboard-edit-popup.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 @NgModule({
   declarations: [
     AppComponent,
     VoxPopComponent,
     PageNotFoundComponent,
     HomeComponent,
-    DashboardComponent
+    DashboardComponent,
+    DashboardEditPopupComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatDialogModule,
+  ],
+  exports: [
+    MatDialogModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
