54698372bf3c9d1acdbfab438f116b954a16b51a
buster scruggs <prettyleetemail@protonmail.com>
2023-05-22 15:45:27 -0400

    Add error catching to dashboard service

--- src/app/dashboard/dashboard.service.ts
+++ src/app/dashboard/dashboard.service.ts
@@ -1,6 +1,6 @@
 import { HttpHeaders, HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, catchError, of } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { Submission } from './dashboard.component';
 
@@ -12,16 +12,22 @@ export class DashboardService {
 
   constructor(private httpClient: HttpClient) { }
   
-  sendReviewedSubmissions(submissions: string): Observable<Object> {
-    return this.httpClient.post(environment.baseUrl + '/dashboard', submissions, {headers: this.httpHeaders})
+  postSubmissions(submissions: string): Observable<string> {
+    return this.httpClient.post(environment.baseUrl + '/dashboard/submit-review', submissions, {responseType: 'text', headers: this.httpHeaders}).pipe(
+      catchError(this.handleError<string>('sendReviewedSubmissions', ''))
+    );
   }
 
   getEnqueuedPosts(): Observable<string> {
-    return this.httpClient.get(environment.baseUrl + '/dashboard/enqueue', {responseType: 'text', headers: this.httpHeaders})
+    return this.httpClient.get(environment.baseUrl + '/dashboard/enqueue', {responseType: 'text', headers: this.httpHeaders}).pipe(
+      catchError(this.handleError<string>('getEnqueuedPosts', ''))
+    );
   }
 
-  getStoredSubmissions(): Observable<string> {
-    return this.httpClient.get(environment.baseUrl + '/dashboard/unreviewed', {responseType: 'text', headers: this.httpHeaders})
+  getUnreviewedSubmissions(): Observable<string> {
+    return this.httpClient.get(environment.baseUrl + '/dashboard/unreviewed', {responseType: 'text', headers: this.httpHeaders}).pipe(
+      catchError(this.handleError<string>('getUnreviewedSubmissions', ''))
+    );
   }
 
   getMostRecentSubmission(sub: Submission): string {
@@ -33,5 +39,20 @@ export class DashboardService {
       return sub.userIP;
     }
     return `No IP`;
+  }
+
+  // shamelessly stolen from angular docs
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+  
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+  
+      // TODO: better job of transforming error for user consumption
+      console.log(`${operation} failed: ${error.message}`);
+  
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
   }
 }
