28e8b54138005db3cbfefd70640380f79a9900b8
hfht <prettyleetemail@proton.com>
2023-05-22 15:43:08 -0400

    Add threejs and types, remodel home page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,15 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template:`
-  <div>
-    <div class="header">
-      <a routerLink="/home" routerLinkActive="active" ariaCurrentWhenActive="page" class="link">Home</a>
-      <a routerLink="/vox-pop" routerLinkActive="active" ariaCurrentWhenActive="page" class="link">Vox Pop</a>
-      <a routerLink="/dashboard" routerLinkActive="active" ariaCurrentWhenActive="page" class="link">Dashboard</a>
+  template: `
+    <div>
+      <router-outlet></router-outlet>
     </div>
-    <router-outlet></router-outlet>
-  </div>`,
+  `,
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
